e237347223076c034dd167f7074d98297bcbc086
Pedro Humberto <78572029+PedroHumberto@users.noreply.github.com>
2023-03-28 19:47:55 -0300

    correções nas entidades

--- src/backend/PB.Domain/Entities/Game.cs
+++ src/backend/PB.Domain/Entities/Game.cs
@@ -15,10 +15,6 @@ namespace PB.Domain.Entities
         public string? Category { get; set; }
         public string? Description { get; set; }
         public byte[]? Images { get; set; }
-<<<<<<< HEAD
         public virtual ICollection<UserGame>? Users { get; set; }
-        
-=======
->>>>>>> 84967c0292841f7a32aa1931cf1a789a6dd05642
     }
 }
--- src/backend/PB.Domain/Entities/Post.cs
+++ src/backend/PB.Domain/Entities/Post.cs
@@ -13,14 +13,9 @@ namespace PB.Domain.Entities
         public int Id { get; set; }
         public string? Title { get; set; }
         public string? PostText { get; set; }
-<<<<<<< HEAD
         public int UserId { get; set; }
         public virtual User? User { get; set; }
         public virtual ICollection<Comment>? Comments { get; set; }
-=======
-        public string? UserName { get; set; }
-        public ICollection<Comment>? Comments { get; set; }
->>>>>>> 84967c0292841f7a32aa1931cf1a789a6dd05642
         public DateTime CreatedAt { get; set; }
     }
 }
